1e69fc3b59b808002f4d4b68b0501d2637a306f1
moses-igbins <moses.igbins@gmail.com>
2023-02-24 10:40:33 -0800

    love

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,22 +1,27 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * print_diagonal - prints hashes squares.
- * @n: number of the diagonals.
- * Return: no return.
+ * print_diagonal - printing lines diagonal
+ * @n: input variable
+ *
+ * Return: void
  */
+
+
 void print_diagonal(int n)
 {
 	int i, j;
 
+	if (n <= 0)
+		_putchar('\n');
 	for (i = 0; i < n; i++)
 	{
-		for (j = 0; j < n; j++)
+		for (j = 0; j < i; j++)
 		{
-			_putchar(35);
+			_putchar(' ');
 		}
-		if (i != n - 1)
-			_putchar('\n');
+		_putchar('\\');
+		_putchar('\n');
 	}
-	_putchar('\n');
+
 }
