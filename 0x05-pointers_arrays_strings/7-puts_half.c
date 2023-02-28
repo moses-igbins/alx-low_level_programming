d1a4c785ee7beaf4e30af3d6ed0c8bcce4f54133
moses-igbins <moses.igbins@gmail.com>
2023-02-27 17:53:21 -0800

    home

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * puts2 - Prints one char out of two of a string.
+ * puts_half - Prints one char out of two of a string.
  * @str: The string containing characters.
  */
-void puts2(char *str)
+void puts_half(char *str)
 {
 	int i, len = 0;
 
