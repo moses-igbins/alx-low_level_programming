80c9ae3d6c7227b8c35e445777e5b42e4148d0f9
moses-igbins <moses.igbins@gmail.com>
2023-03-06 09:44:34 -0800

    more

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,36 +1,42 @@
 #include "main.h"
 /**
- * _strstr - Function that locates a substring.
+ * _strstr - function that locates a substring.
+ *@haystack: first value -char
+ *@needle: second value - char
  *
- * @needle: char pointer
- * @haystack: char pointer
- *
- *
- * Return: s
+ * Return: char with result
  */
-
 char *_strstr(char *haystack, char *needle)
 {
-	unsigned int count;
-	for (count = 0; needle[count] && haystack[count]; count++)
-
-		for (; needle[count] != haystack[count]; haystack++)
-			count = 0;
-		if(needle[count])
-
-			return (0);
-
-		else
-			return (char *)haystack;
-
-	int count;
-
-	for (; haystack[0]; haystack++)
+	int count = 0, a = 0, k, j = 0, l, i = 0;
+	char *p;
+	while (needle[count] != '\0')
 	{
-		for (count = 0; haystack[count] == needle[count]; count++)
-			;
-		if (!(needle[count]))
-			return (haystack);
+		count++;
 	}
-	return (0);
+	while (haystack[i] != '\0')
+	{
+		i++;
+			}
+	if (count == 0)
+		return (haystack);
+	while (haystack[j] != '\0')
+	{
+		if (haystack[j] == needle[0])
+		{
+			p = &haystack[j];
+			l = j;
+			a = 0;
+			for (k = 0; k < count; k++)
+			{
+				if (haystack[l] == needle[k])
+					a++;
+				l++;
+			}
+		}
+		if (a == count)
+			return (p);
+		j++;
+	}
+	return ('\0');
 }
